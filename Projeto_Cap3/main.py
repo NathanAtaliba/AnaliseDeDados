b65a4ca00b45529ba393612cde3aeaa07480c71d
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-16 20:30:43 -0300

    Exercicios

--- Projeto_Cap3/main.py
+++ Projeto_Cap3/main.py
@@ -36,4 +36,34 @@ pessoa = {
          }
 print(pessoa)
 '''
+#EXERCICIOS:
+#1)
+selecoes = ['Real Madrid','Barcelona','Corinthians','Bayer de Munchen','Al Ahly']
+print(selecoes[:3]) #PRINT DOS TRES PRIMEIROS COLOCADOS
+print(selecoes[3:]) #PRINT DOS DOIS ULTIMOS COLOCADOS
+selecoes.sort()  #ORGANIZANDO ITENS EM ORDEM ALFABETICA
+print(selecoes) #PRINT DA LISTA COM ITENS ORDENADOS
+print(selecoes.index('Barcelona')) #PRINT POSIÇÃO DO BARCELONA NA LISTA
+
+#2)
+loja1 = {'Galaxy','Xiaomi'}
+loja2 = {'Apple','Motorola'}
+print('O total de itens da loja 1 é:' , len(loja1))
+print('Voce podera comprar na loja 1, celulares das marcas: ', loja1)
+print('O total de itens da loja 2 é:' ,len(loja2))
+print('Voce podera comprar na loja 2, celulares das marcas: ', loja2)
+
+#3)
+numero = input('Entre com o numero de alunos: ')
+DicionarioAlunos = {
+                    #'nome': 'nome',
+                    #'nota': 'nota',
+                    #'situacao': 'situacao'
+                    }
+for i in numero:
+    nome = input('Entre com o nome do aluno:')
+    nota = input('Entre com a nota do aluno:')
+    DicionarioAlunos
+
+
 
