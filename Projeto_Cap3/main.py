0cc0142a5df4940ae14b323751f79226eda7c022
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-03-18 17:31:37 -0300

    Exercicios

--- Projeto_Cap3/main.py
+++ Projeto_Cap3/main.py
@@ -54,16 +54,20 @@ print('O total de itens da loja 2 é:' ,len(loja2))
 print('Voce podera comprar na loja 2, celulares das marcas: ', loja2)
 
 #3)
-numero = input('Entre com o numero de alunos: ')
-DicionarioAlunos = {
-                    #'nome': 'nome',
-                    #'nota': 'nota',
-                    #'situacao': 'situacao'
-                    }
-for i in numero:
+numero = int(input('Entre com o numero de alunos: '))
+DicAlunos = {} #dicionario de alunos
+for i in range(numero):
+    aluno = {}
     nome = input('Entre com o nome do aluno:')
+    aluno['name'] = nome
     nota = input('Entre com a nota do aluno:')
-    DicionarioAlunos
+    aluno['nota'] = nota
+    DicAlunos[i] = aluno
 
-
+for i in range(len(DicAlunos)):
+    if DicAlunos[i]['nota'] >= '60':
+        DicAlunos[i]['Situacao'] = 'AP'
+    else:
+        DicAlunos[i]['Situacao'] = 'RP'
+print(DicAlunos)
 
