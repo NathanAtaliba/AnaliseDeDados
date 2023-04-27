6cde7b86f84f38bc682ba82de0dcddde7b702831
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-04-26 22:29:41 -0300

    Slicing_Prova

---
+++ SlicingProva/main.py
@@ -0,0 +1,23 @@
+import numpy as np
+'''
+np.random.seed(10)
+mtz = np.random.randint(1, 10, 16).reshape(4, 4)
+print(mtz)
+#CONDICIONAIS DO NUMPY
+print(mtz % 2 == 1)#Extraindo Trues e Falses
+print(mtz[mtz % 2 == 1]) #Extraindo os numeros impares]
+print(mtz[mtz >= 3]) #Extraindo os numeros maiores ou iguais a 3
+'''
+'''
+#MANIPULANDO TEXTOS COM SUBPACOTE CHAR
+arr = np.array(['Mateus',' Vinix', 'Leticia','Raphael','Livia','Nathan'])
+print(np.char.find(arr, 'ia') > 0) #PROCURANDO IA NA PALAVRA
+cond = np.char.find(arr, 'ia') > 0
+print(arr[cond]) #
+'''
+#TRABALHANDO COM DATA SETS
+dataset = np.loadtxt('space.csv',
+                     delimiter=';',
+                     dtype=str,
+                     encoding='utf-8')
+
